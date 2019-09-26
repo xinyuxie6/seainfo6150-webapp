c726427faff9a5b320b147a9270356157c9eb082
Xinyu Xie <xie.xinyu@husky.neu.edu>
2019-09-26 16:23:49 -0700

    finish assignment4

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -5,11 +5,18 @@ const ArticleList = props => {
   return (
     <ul>
       {props.articles.map(article => (
-        <li key={article.slug}>ArticleListItem component goes here</li>
+        <li key={article.slug}>
+        <h3>{article.title}</h3>
+        <p>{article.shortText}</p>
+        <time dateTime={article.pubDate}>{article.pubDate}</time><button onClick={() => alert(`${article.slug}`)}>Show article slug</button>
+        </li>
       ))}
+
     </ul>
   );
 };
+
+
 
 ArticleList.propTypes = {
   articles: PropTypes.array.isRequired
