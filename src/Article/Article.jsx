65641c2baf9bc45e922b2b8f13da17776213e384
Xinyu Xie <xie.xinyu@husky.neu.edu>
2019-09-27 13:27:12 -0700

    revised HTML followed by TA's advice

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -5,29 +5,35 @@ const Article = () => {
     <html>
     <head>
       <meta charset="utf-8"/>
-      <title>article</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>Article</title>
     </head>
 
     <body>
+      <article>
       <div>
       <h1>The Statue of Liberty's torch heads to new museum</h1>
-
       
-      <p><address><em>by Kate Farley (<a href="mailto:kate.farley@nytimes.com">kate.farley@nytimes.com</a>)</em></address><br/><time>November 22, 2018</time></p>
+      <address>
+        <em>by Kate Farley (<a href="mailto:kate.farley@nytimes.com">kate.farley@nytimes.com</a>)</em>
+      </address>
+      <time dateTime="2018-11-22">November 22, 2018</time>
+      
 
-      <span>The Statue of Liberty's original torch is getting a new home.</span>
+      <p>The Statue of Liberty's original torch is getting a new home.</p>
 
       <p>Made of copper and gold so that it would shine bright, the first torch took a beating from the weather and was replaced by a replica in 1984. It's been stored in the statue's pedestal ever since.</p>
 
       <p>But visitors will be able to see the original 3,600-pound torch in all its glory at a new Statue of Liberty Museum, which will open on Liberty Island in May 2019. The torch was transported there by truck last week, along with a replica model of Lady Liberty's face.</p>
 
-      <p>"Although it is not one of the most difficult things we have ever moved," says Douglas Phelps, who oversaw the relocation of the torch, "it is certainly the most important."</p>
+      <p><q>Although it is not one of the most difficult things we have ever moved, </q>says Douglas Phelps, who oversaw the relocation of the torch, <q>it is certainly the most important.</q></p>
       </div>
 
       <div>
       <h1>The history of the Statue of Liberty</h1>
 
-      <p>Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation of <em>Outstanding Universal Value</em>.</p>
+      <p>Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation of <i>Outstanding Universal Value</i>.</p>
 
       <p>Her full name is Liberty Enlightening the World, and she was designed by Frederic Auguste Bartholdi and built by Gustave Eiffel -- yes, the same guy who built Paris's Eiffel Tower.</p>
 
@@ -35,7 +41,7 @@ const Article = () => {
 
       <p>Since then, the statue has served as a symbol of hope and possibility for many people who entered the United States as immigrants via neighboring Ellis Island.</p>
 
-      <p>"The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy," says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit inspired the decision to inscribe these famous lines on the statue's pedestal: </p>
+      <p><q>The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy,</q> says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit inspired the decision to inscribe these famous lines on the statue's pedestal: </p>
       <blockquote>Give me your tired, your poor <br/> Your huddled masses yearning to breathe free.</blockquote>
 
       <p>In 2017, nearly 4.5 million people visited Liberty Island.</p>
@@ -57,6 +63,7 @@ const Article = () => {
       <li>Total weight of the Statue of Liberty is <strong>225 tons (or 450,000 pounds)</strong></li>
       </ul>
       </div>
+      </article>
     </body>
     </html>
   )
