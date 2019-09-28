05e13590d8eb820990fa909b8cb3380c6a33a03c
Xinyu Xie <xie.xinyu@husky.neu.edu>
2019-09-27 21:53:15 -0700

    add ArticleListItem

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import PropTypes from 'prop-types'
+import ArticleListItem from "./ArticleListItem"
+
 
 const ArticleList = props => {
   return (
     <ul>
       {props.articles.map(article => (
         <li key={article.slug}>
-        <h3>{article.title}</h3>
-        <p>{article.shortText}</p>
-        <time dateTime={article.pubDate}>{article.pubDate}</time><button onClick={() => alert(`${article.slug}`)}>Show article slug</button>
+          <ArticleListItem article={article}/>
         </li>
       ))}
-
     </ul>
   );
 };
-
-
 
 ArticleList.propTypes = {
   articles: PropTypes.array.isRequired
